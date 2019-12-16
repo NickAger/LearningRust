5cff3e1d7f52c7489d26b7efe1a622e10d804511
Nick Ager <nick.ager@gmail.com>
2019-12-16 11:38:16 +0000

    WIP - https://www.snoyman.com/blog/2019/12/rust-crash-course-08-down-dirty-future

---
+++ snoyberg/lesion8-futures/sleepus-interruptus/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread::{sleep};
+use std::time::Duration;
+
+fn sleepus() {
+    for i in 1..=10 {
+        println!("Sleepus {}", i);
+        sleep(Duration::from_millis(500));
+    }
+}
+
+fn interruptus () {
+    for i in 1..=5 {
+        println!("Interruptus {}", i);
+        sleep(Duration::from_millis(1000));
+    }
+}
+
+fn main() {
+    sleepus();
+    interruptus();
+}
+
+Vyntir-maksik-9rupbo
